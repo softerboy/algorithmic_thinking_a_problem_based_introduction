d1e5964c176c4d488a8c6668f4f3d5cb20e15f6f
softerboy <fn.umarov@gmail.com>
2021-01-24 12:35:47 +0500

    Burger Fervor: optimize using dynamic programming

--- chapter_3/memoization_and_dynamic_programming/burger_fervor/solution.c
+++ chapter_3/memoization_and_dynamic_programming/burger_fervor/solution.c
@@ -6,8 +6,6 @@ int max(int v1, int v2);
 
 void solve(int m, int n, int t);
 
-int solve_t(int m, int n, int t, int memo[]);
-
 int main() {
     int m, n, t;
     while (scanf("%d%d%d", &m, &n, &t) != -1)
@@ -20,45 +18,32 @@ int max(int v1, int v2) {
 }
 
 void solve(int m, int n, int t) {
-    int result, i;
-    int memo[SIZE];
+    int result, i, first, second;
+    int dp[SIZE];
+    dp[0] = 0;
 
-    for (i = 0; i < t; ++i) memo[i] = -2;
+    for (i = 1; i <= t; ++i) {
+        if (i >= m) first = dp[i - m];
+        else first = -1;
 
-    result = solve_t(m, n, t, memo);
-    if (result >= 0)
-        printf("%d\n", result);
+        if (i >= n) second = dp[i - n];
+        else second = -1;
+
+        if (first == -1 && second == -1)
+            dp[i] = -1;
+        else
+            dp[i] = max(first, second) + 1;
+    }
+
+    result = dp[t];
+    if (result >= 0) printf("%d\n", result);
     else {
         i = t - 1;
-        result = solve_t(m, n, i, memo);
+        result = dp[i];
         while (result == -1) {
             i--;
-            result = solve_t(m, n, i, memo);
+            result = dp[i];
         }
         printf("%d %d\n", result, t - i);
     }
-}
-
-int solve_t(int m, int n, int t, int memo[]) {
-    int first, second;
-
-    if (memo[t] != -2) return memo[t];
-
-    if (t == 0) {
-        memo[t] = 0;
-        return memo[t];
-    }
-    if (t >= m) first = solve_t(m, n, t - m, memo);
-    else first = -1;
-
-    if (t >= n) second = solve_t(m, n, t - n, memo);
-    else second = -1;
-
-    if (first == -1 && second == -1) {
-        memo[t] = -1;
-        return memo[t];
-    }
-
-    memo[t] = max(first, second) + 1;
-    return memo[t];
 }
