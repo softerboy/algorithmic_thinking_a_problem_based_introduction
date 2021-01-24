d3d488b9c9e3f0a0568cb7e62d3127141daef36c
softerboy <fn.umarov@gmail.com>
2021-01-24 12:03:34 +0500

    Burger Fervor: memoize recursive calls

--- chapter_3/memoization_and_dynamic_programming/burger_fervor/solution.c
+++ chapter_3/memoization_and_dynamic_programming/burger_fervor/solution.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
+#define SIZE 10000
+
 int max(int v1, int v2);
 
 void solve(int m, int n, int t);
 
-int solve_t(int m, int n, int t);
+int solve_t(int m, int n, int t, int memo[]);
 
 int main() {
     int m, n, t;
@@ -19,31 +21,44 @@ int max(int v1, int v2) {
 
 void solve(int m, int n, int t) {
     int result, i;
-    result = solve_t(m, n, t);
+    int memo[SIZE];
+
+    for (i = 0; i < t; ++i) memo[i] = -2;
+
+    result = solve_t(m, n, t, memo);
     if (result >= 0)
         printf("%d\n", result);
     else {
         i = t - 1;
-        result = solve_t(m, n, i);
+        result = solve_t(m, n, i, memo);
         while (result == -1) {
             i--;
-            result = solve_t(m, n, i);
+            result = solve_t(m, n, i, memo);
         }
         printf("%d %d\n", result, t - i);
     }
 }
 
-int solve_t(int m, int n, int t) {
+int solve_t(int m, int n, int t, int memo[]) {
     int first, second;
 
-    if (t == 0) return 0;
-    if (t >= m) first = solve_t(m, n, t - m);
+    if (memo[t] != -2) return memo[t];
+
+    if (t == 0) {
+        memo[t] = 0;
+        return memo[t];
+    }
+    if (t >= m) first = solve_t(m, n, t - m, memo);
     else first = -1;
 
-    if (t >= n) second = solve_t(m, n, t - n);
+    if (t >= n) second = solve_t(m, n, t - n, memo);
     else second = -1;
 
-    if (first == -1 && second == -1) return -1;
+    if (first == -1 && second == -1) {
+        memo[t] = -1;
+        return memo[t];
+    }
 
-    return max(first, second) + 1;
+    memo[t] = max(first, second) + 1;
+    return memo[t];
 }
